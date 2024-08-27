ad11128741f5ff0a41b1355e69f36dcf10f10285
fat83dotcom <fernando.quimic@gmail.com>
2024-08-27 10:51:35 -0300

    Reaftoração e novos elementos de log.

--- article_crawler.py
+++ article_crawler.py
@@ -12,10 +12,30 @@ from automation_db import DBConnection
 from typing import Any
 
 
+class Log:
+    def register_log(self, path: str, payload: str):
+        with open(path, 'a+', encoding='utf-8') as file:
+            file.write(f'{payload} - Data/Hora: {datetime.now()}\n')
+
+
+class Message():
+    log = Log()
+
+    @classmethod
+    def send_message(self, message: str, method: str, class_) -> str:
+        mess: str = f'''
+            Error: {message} \n
+            Method: {method} \n
+            Class: {class_} \n
+        '''
+        self.log.register_log('/search_app_logs/log_error.txt', mess)
+
+
 class Content:
     def __init__(
-        self, title: str, article_content: str, origin_link: str
+        self, title: str, article_content: str, origin_link: str, id_=-1
     ) -> None:
+        self.id = id_
         self.title = title
         self.article_content = article_content
         self.origin_link = origin_link
@@ -23,7 +43,7 @@ class Content:
     def __iter__(self):
         return (
             i for i in (
-                self.title, self.article_content, self.origin_link
+                self.id, self.title, self.article_content, self.origin_link
             )
         )
 
@@ -61,59 +81,51 @@ class DataHandler:
         try:
             changed_data: list = []
             id_counter: int = 0
-            for dicts in target_data:
-                dicts.update({'id': id_counter})
-                changed_data.append(dicts)
-                id_counter += 1
+            for list_content in target_data:
+                for content in list_content:
+                    # content.update({'id': id_counter})
+                    content.__dict__['id'] = id_counter
+                    changed_data.append(content)
+                    id_counter += 1
         except Exception as e:
-            raise e
+            Message.send_message(e, 'insert_id', self.__class__)
         return changed_data
 
     def reinsert_link(
-        self, dict_with_link: list, dict_from_ia: list
+        self, content_with_link: list, content_from_ia: list
     ) -> list:
         try:
             result: list = []
-            for content_a in dict_with_link:
-                for content_b in dict_from_ia:
-                    if content_a['id'] == content_b['id'] \
+            for content_a in content_with_link:
+                for content_b in content_from_ia:
+                    if content_a.id == content_b['id'] \
                             and content_b['article_content'] != '':
-                        content_b['origin_link'] = content_a['origin_link']
-                        content_b['title'] = content_a['title']
-                        result.append(content_b)
+                        content_a.article_content = content_b['article_content']
+                        result.append(content_a)
         except Exception as e:
-            raise e
+            Message.send_message(e, 'reinsert_link', self.__class__)
         return result
 
-    def unpack_data_content(self, packaged_data: list) -> list:
-        try:
-            unpack: list = []
-            for pack in packaged_data:
-                for content in pack:
-                    unpack.append(content.__dict__)
-        except Exception as e:
-            raise e
-        return unpack
-
-    def dictionary_key_separator(self, payload: dict, target_key: str) -> list:
+    def prepare_content_for_ia(self, content: Content) -> list:
         try:
             chosen_keys: list = []
-            for load in payload:
+            for attrs in content:
                 target: dict = {}
-                for k, v in load.items():
-                    if k == target_key:
-                        target[k] = v
-                        target['id'] = load['id']
+                if attrs.article_content != '':
+                    target['article_content'] = attrs.article_content
+                    target['id'] = attrs.id
                 chosen_keys.append(target)
         except Exception as e:
-            raise e
+            Message.send_message(
+                e, 'prepare_content_for_ia', self.__class__
+            )
         return chosen_keys
 
     def text_converter(self, text: str) -> Any:
         try:
             return ast.literal_eval(text)
         except Exception as e:
-            raise e
+            Message.send_message(e, 'text_converter', self.__class__)
 
 
 class IATextSummarizer:
@@ -122,7 +134,7 @@ class IATextSummarizer:
             genai.configure(api_key=os.getenv("API_KEY"))
             self.model = genai.GenerativeModel(ia_engine)
         except Exception as e:
-            raise e
+            Message.send_message(e, '__init__', self.__class__)
 
     def summarize_text(self, prompt: str, text: str) -> str:
         '''O prompt deve instruir à IA como resumir a lista de dicionários
@@ -133,7 +145,7 @@ class IATextSummarizer:
                 f"""{prompt} : {text}"""
             )
         except Exception as e:
-            raise e
+            Message.send_message(e, 'summarize_text', self.__class__)
         return response.text
 
 
@@ -144,14 +156,14 @@ class HtmlGenerator:
             for content in target:
                 result += f'''
 <div class="exo-2-font">
-<strong><b>{content["title"]}:</b></strong>
-<p>{content["article_content"]}</p>
-<a href="{content["origin_link"]}" target="_blank" rel="noopener noreferrer">Saiba mais...</a>
+<strong><b>{content.title}:</b></strong>
+<p>{content.article_content}</p>
+<a href="{content.origin_link}" target="_blank" rel="noopener noreferrer">Saiba mais...</a>
 <hr>
 </div>\n
 '''
         except Exception as e:
-            raise e
+            Message.send_message(e, 'generator', self.__class__)
         return result
 
 
@@ -159,9 +171,9 @@ class Crawler:
     def __get_page(self, url: str) -> BeautifulSoup | None:
         try:
             html = requests.get(url, allow_redirects=True)
-            return BeautifulSoup(html.text, 'html.parser')
-        except Exception:
-            return None
+        except Exception as e:
+            return Message.send_message(e, '__get_page', self.__class__)
+        return BeautifulSoup(html.text, 'html.parser')
 
     def __get_safe(self, bs: BeautifulSoup, selector: str) -> str:
         try:
@@ -170,7 +182,7 @@ class Crawler:
                 return child[0].get_text()
             return ''
         except Exception as e:
-            raise e
+            Message.send_message(e, '__get_safe', self.__class__)
 
     def __get_from_attr(self, bs: BeautifulSoup, attr: str) -> list:
         try:
@@ -179,6 +191,19 @@ class Crawler:
             ]
         except Exception:
             return []
+
+    def __get_page_change_search_topic(self, site: WebSite):
+        try:
+            url = ''
+            if site.topic == '':
+                url = site.search_url
+            else:
+                url = site.search_url + site.topic
+        except Exception as e:
+            Message.send_message(
+                e, '__get_page_change_search_topic', self.__class__
+            )
+        return self.__get_page(url)
 
     def __select_random_link(self, links: list, number_links: int) -> list:
         seed(time())
@@ -192,18 +217,9 @@ class Crawler:
                 links[randint(0, (lentgh - 1))] for _ in range(number_links)
             ]
         except Exception as e:
-            raise e
-
-    def __get_page_change_search_topic(self, site: WebSite):
-        try:
-            url = ''
-            if site.topic == '':
-                url = site.search_url
-            else:
-                url = site.search_url + site.topic
-        except Exception as e:
-            raise e
-        return self.__get_page(url)
+            Message.send_message(
+                e, '__select_random_link', self.__class__
+            )
 
     def __parse_unquote_url(self, url: str, site: WebSite) -> str:
         '''
@@ -220,7 +236,9 @@ class Crawler:
                 return urllib.parse.unquote(match.group(1))
             return url
         except Exception as e:
-            raise e
+            Message.send_message(
+                e, '__parse_unquote_url', self.__class__
+            )
 
     def __target_pages_engine(self, site: WebSite, target_url: str) -> Content:
         try:
@@ -229,7 +247,9 @@ class Crawler:
             title = self.__get_safe(bs, site.title_selector)
             content = self.__get_safe(bs, site.content_selector)
         except Exception as e:
-            raise e
+            Message.send_message(
+                e, '__target_pages_engine', self.__class__
+            )
         return Content(title, content, url)
 
     def __get_target_pages(
@@ -245,40 +265,38 @@ class Crawler:
                     url = site.link_complement + link
                 result.append(self.__target_pages_engine(site, url))
             except Exception as e:
-                raise e
+                Message.send_message(
+                    e, '__get_target_pages', self.__class__
+                )
         return result
 
     def search(self, site: WebSite) -> list[Content]:
         try:
-            bs = self.__get_page_change_search_topic(site)
-            target_links = bs.select(site.search_result_selector)
+            list_of_pages = self.__get_page_change_search_topic(site)
+            target_links = list_of_pages.select(site.search_result_selector)
             links = self.__get_from_attr(target_links, 'href')
             select_links = self.__select_random_link(links, site.n_links)
         except Exception as e:
-            raise e
+            Message.send_message(
+                    e, 'search', self.__class__
+                )
         return self.__get_target_pages(site, select_links)
 
 
-class PostDataInsert:
+class Post:
     seed(time())
     date = datetime.now()
     date_fmt = date.strftime('%d/%m/%y')
 
     def __init__(self, content: str) -> None:
         self.title: str = f'News Letter - {self.date_fmt}'
-        self.excerpt: str = 'Notícias do dia selecionadas para você'
+        self.excerpt: str = 'Notícias do dia selecionadas para você.'
         self.content = content
-        self.create_at: datetime = datetime.now()
-        self.created_by: int = 2
-        self.categoy_key: int = 2
+        self.createdAt: datetime = datetime.now()
+        self.createdBy_id: int = 2
+        self.categoryKey_id: int = 2
         self.slug: str = f'news-letter-{randint(1000, 1000000)}-bot'
-        self.is_publish: bool = False
-
-
-class Log:
-    def register_log(self, path: str, payload: str):
-        with open(path, 'a+', encoding='utf-8') as file:
-            file.write(f'{payload}\n')
+        self.isPublished: bool = False
 
 
 class Bot:
@@ -301,7 +319,7 @@ class Bot:
         As chaves devem ser separadas por virgulas para cada ocorrencia,
         exatamente como uma lista de dicionários Python. A saida deve ser
         exatamente assim.
-            '''
+        '''
         except Exception:
             ...
 
@@ -325,46 +343,34 @@ class Bot:
             # busca os dados atraves das instancias WebSite
             pipe_line_3: list = [self.crawler.search(s) for s in pipe_line_2]
 
-            # Desempacota os dados obtidos pela busca
-            pipe_line_4 = self.d_handler.unpack_data_content(pipe_line_3)
-
             # insere um identificador para sincronização posterior dos dados
-            pipe_line_5 = self.d_handler.insert_id(pipe_line_4)
+            pipe_line_4 = self.d_handler.insert_id(pipe_line_3)
 
             # separa o texto e o id preparando para o processo de IA
-            pipe_line_6 = self.d_handler.dictionary_key_separator(
-                pipe_line_5, 'article_content'
+            pipe_line_5 = self.d_handler.prepare_content_for_ia(
+                pipe_line_4
             )
 
             # Dados passam pela IA para resumo dos textos
-            pipe_line_7 = self.ia_sumerizer.summarize_text(
-                self.prompt, str(pipe_line_6)
+            pipe_line_6 = self.ia_sumerizer.summarize_text(
+                self.prompt, str(pipe_line_5)
             )
 
             # Dados recebidos da IA são convertidos em carga util
-            pipe_line_8 = self.d_handler.text_converter(pipe_line_7)
+            pipe_line_7 = self.d_handler.text_converter(pipe_line_6)
 
             # Reinserir os links em seus respectivos lugares
-            pipe_line_9 = self.d_handler.reinsert_link(
-                pipe_line_4, pipe_line_8
+            pipe_line_8 = self.d_handler.reinsert_link(
+                pipe_line_4, pipe_line_7
             )
 
             # Formatar os dados no formato HTML para serem inseridos
-            pipe_line_10 = self.gen_html.generator(pipe_line_9)
+            pipe_line_9 = self.gen_html.generator(pipe_line_8)
 
             #  Inserir os dados, como um post no blog. Fim do ALgoritmo
-            content = PostDataInsert(pipe_line_10).__dict__
-            insert_data: dict = {
-                'title': content['title'],
-                'excerpt': content['excerpt'],
-                'content': content['content'],
-                'createdAt': content['create_at'],
-                'createdBy_id': content['created_by'],
-                'categoryKey_id': content['categoy_key'],
-                'slug': content['slug'],
-                'isPublished': content['is_publish']
-            }
-            self.db.insert_table('post', insert_data)
+            content = Post(pipe_line_9).__dict__
+
+            self.db.insert_table('post', content)
         except Exception as e:
             self.log.register_log(
                 '/search_app_logs/log_1.txt',
@@ -375,3 +381,4 @@ class Bot:
 if __name__ == '__main__':
     main = Bot()
     main.run()
+
